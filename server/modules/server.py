67b076ca482d3671aeb72ddeda52242d1ae3f37f
TSM <ts.marinov@gmail.com>
2024-02-11 00:19:29 +0200

    grafana fix, server reconnect (address already used), ignore dew

--- server/modules/server.py
+++ server/modules/server.py
@@ -1,6 +1,6 @@
 # import os
 from .config import Config
-import json
+# import json
 import signal
 import socket
 import ssl
@@ -8,13 +8,11 @@ import sys
 import jwt
 from .helpers import log_time_zone
 from .db import WeatherDatabase
+import time
 
 
 class WeatherServer:
-    def __init__(
-            self,
-            root_folder
-    ):
+    def __init__(self, root_folder):
         self.config = Config(root_folder)
 
     def start(self):
@@ -27,25 +25,48 @@ class WeatherServer:
             print("Server stopped")
             sys.exit(0)
 
-        # Create a socket and listen for connections
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port = self.config.get_port()
+        def bind_socket(host_value, port_value):
+            attempt = 0
+            max_attempts = 10  # Maximum number of attempts
+            delay = 10  # Delay in seconds between attempts
+
+            while True:
+                try:
+                    # Create a socket and listen for connections
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock.bind((host_value, port_value))
+                    sock.listen()
+
+                    # Wrap the socket with SSL
+                    wrapped_socket_ssl = ssl.wrap_socket(
+                        sock,
+                        keyfile=self.config.get_certificate_key(),
+                        certfile=self.config.get_certificate(),
+                        server_side=True
+                    )
+
+                    print(f"{log_time_zone()}: Server is listening on {host_value}:{port_value}")
+                    print()
+                    # Register the signal handler function
+                    signal.signal(signal.SIGINT, handle_signal)
+
+                    return wrapped_socket_ssl
+
+                except OSError as e:
+                    if e.errno == 98 and attempt < max_attempts:
+                        print(f"Address already in use. Retrying in {delay} seconds...")
+                        attempt += 1
+                        time.sleep(delay)
+                    else:
+                        print("Failed to start server after multiple attempts. Exiting.")
+                        sys.exit(1)
+
+        # Get host and port from configuration
         host = self.config.get_host()
-        sock.bind((host, port))
-        sock.listen()
+        port = self.config.get_port()
 
-        # Wrap the socket with SSL
-        wrapped_socket = ssl.wrap_socket(
-            sock,
-            keyfile=self.config.get_certificate_key(),
-            certfile=self.config.get_certificate(),
-            server_side=True
-        )
-
-        print(f"{log_time_zone()}: Server is listening on {host}:{port}")
-        print()
-        # Register the signal handler function
-        signal.signal(signal.SIGINT, handle_signal)
+        # Bind the socket
+        wrapped_socket = bind_socket(host, port)
 
         while True:
             conn, addr = wrapped_socket.accept()
@@ -88,10 +109,8 @@ class WeatherServer:
                         if not clients.get(device):
                             conn.sendall(b'Invalid device')
                         else:
-                            # print(payload['data'])
                             # json_str_pretty = json.dumps(payload['data'], indent=4)
                             # print(json_str_pretty)
-                            # sys.exit()
                             status = weather_db.add_data(device, payload['data'])
                             if status.get('status'):
                                 conn.sendall(b'OK')
